e2c6f12286e9698718490e5d695497c184dc8269
Adrian Chang <amchang87@gmail.com>
2011-09-07 22:41:02 -0700

    migrate

--- src/collection/photo.js
+++ src/collection/photo.js
@@ -1,7 +1,17 @@
 AC.Collection.Photo = Backbone.Collection.extend({
+	// the current fetch options
+	currentFetchOptions: null,
 	model: AC.Model.Photo,
+	/**
+	 * Generate a set of fetch options
+	 **/
+	generateFetchOptions: function(text, page) {
+
+	},
 	initialize: function() {
+
 		// every collection has a custom set of ajaxOptions, don't share
+	
 	},
 	ajaxOptions: {
 		url: 'http://api.flickr.com/services/rest/',
